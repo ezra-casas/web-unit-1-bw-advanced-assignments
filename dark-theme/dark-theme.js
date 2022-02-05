504a2abaeaad8c45cf16bf2348f0f1efa5afc93e
Ezra C <ezra.casas@ctemplar.com>
2022-02-05 09:51:47 -0700

    Completed project.

--- dark-theme/dark-theme.js
+++ dark-theme/dark-theme.js
@@ -1,1 +1,14 @@
 // you need to create DOM selectors to grab the correct HTML elements and update their styles to the opposite theme's css classes when you want to toggle the theme off and on
+let black = "black"
+let white = "white"
+let color = white;
+function changeTheme() {
+    const element = document.getElementById("content")
+    if(color === white) {
+        color = black;
+    }else{
+        color = white;
+    }
+    element.style.backgroundColor = `${color}`
+
+}
